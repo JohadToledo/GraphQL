3c743eb8ab07676481ff624b934857339fd607fb
johad <johadtoledo@gmail.com>
2022-04-12 20:41:43 +0200

    add some validations

---
+++ graphql/resolvers/users.js
@@ -0,0 +1,56 @@
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const { UserInputError } = require('apollo-server')
+
+const { validateRegisterInput } = require('../../util/validators')
+const { SECRET_KEY } = require('../../config')
+const User = require('../../models/User')
+
+module.exports = {
+    Mutation:{
+        async register(
+            _,
+             {
+                 registerInput: { username, email, password, confirmPassword}
+             },
+        ){
+            // Validate user data
+            const { valid, errors } = validateRegisterInput(username, email, password, confirmPassword)
+            if(!valid){
+                throw new UserInputError('Errors', { errors })
+            }
+            // TODO: make sure user doesnt already exist 
+            const user = await User.findOne({ username })
+            if(user){
+                throw new UserInputError('Username is taken',{
+                    errors:{
+                        username: 'this username is taken'
+                    }
+                })
+            }
+            // hash password and create an auth token
+            password = await bcrypt.hash(password, 12)
+
+            const newUser = new User({
+                email,
+                username,
+                password,
+                createdAt: new Date().toISOString()
+            })
+
+            const res = await newUser.save()
+
+            const token = jwt.sign({
+                id: res.id,
+                email: res.email,
+                username: res.username
+            }, SECRET_KEY, { expiresIn: '1h'})
+
+            return {
+                ...res._doc,
+                id: res._id,
+                token
+            }
+        }
+    }
+}
