e94b70e59efb9f324c50a9d74b1962e78bc0b188
johad <johadtoledo@gmail.com>
2022-04-12 21:34:04 +0200

    ADD login mutation and validation for login as well

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -2,12 +2,48 @@ const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const { UserInputError } = require('apollo-server')
 
-const { validateRegisterInput } = require('../../util/validators')
+const { validateRegisterInput, validateLoginInput } = require('../../util/validators')
 const { SECRET_KEY } = require('../../config')
 const User = require('../../models/User')
 
+const generateToken = (user) =>{
+    return jwt.sign({
+        id: user.id,
+        email: user.email,
+        username: user.username
+    }, SECRET_KEY,
+     { expiresIn: '1h'})
+}
+
 module.exports = {
     Mutation:{
+        async login(_, {username, password}){
+            const {errors, valid} = validateLoginInput(username, password)
+
+            if(!valid){
+                throw new UserInputError('Not valid', { errors })
+            }
+            const user = await User.findOne({username})
+
+            if(!user){
+                errors.general = 'User not found'
+                throw new UserInputError('User not found', { errors })
+            }
+
+            const match = await bcrypt.compare(password, user.password)
+            if(!match){
+                errors.general = 'Wrong credentials'
+                throw new UserInputError('Wrong credentials', { errors })
+            }
+
+            const token = generateToken(user)
+
+            return {
+                ...user._doc,
+                id: user._id,
+                token
+            }
+        },
         async register(
             _,
              {
@@ -40,11 +76,7 @@ module.exports = {
 
             const res = await newUser.save()
 
-            const token = jwt.sign({
-                id: res.id,
-                email: res.email,
-                username: res.username
-            }, SECRET_KEY, { expiresIn: '1h'})
+            const token = generateToken(res)
 
             return {
                 ...res._doc,
