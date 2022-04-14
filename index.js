c9723fb382a40d1c998b2ff49a3f57419ec7bba6
johad <johadtoledo@gmail.com>
2022-04-14 12:57:22 +0200

    ADD likes and comments count

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const { ApolloServer } = require('apollo-server')
+const { PubSub } =  require('graphql-subscriptions');
+
 const mongoose = require('mongoose')
 
 const typeDefs = require('./graphql/typeDefs')
@@ -6,10 +8,12 @@ const resolvers = require('./graphql/resolvers')
 const { MONGODB } = require('./config.js')
 
 
+const pubsub = new PubSub()
+
 const server = new ApolloServer({
     typeDefs,
     resolvers,
-    context: ({ req }) => ({ req })
+    context: ({ req }) => ({ req, pubsub })
 })
 
 mongoose.connect(MONGODB, { useNewUrlParser: true })
