cbb8c4ce592f2d1099738d7fe761e60404d8dbb6
johad <johadtoledo@gmail.com>
2022-04-15 14:41:39 +0200

    ADD semantic ui and routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,33 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Container } from 'semantic-ui-react';
 
 import './App.css';
-
-function App() {
-  return (
-    <div>
-      <h1>Hi World!</h1>
-    </div>
-  );
-}
-
-export default App;
+import MenuBar from './components/MenuBar';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+<link
+    async
+    rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/semantic-ui@2/dist/semantic.min.css"
+  />
+  
+  function App() {
+    return (
+      <BrowserRouter>
+       <Container>
+        <MenuBar />
+        <Routes>
+          <Route exact path="/" element={<Home />}></Route>
+          <Route exact path="/login" element={<Login />}></Route>
+          <Route exact path="/register" element={<Register />}></Route>
+        </Routes>
+       </Container>
+      
+      
+      </BrowserRouter>
+      );
+    }
+    
+    export default App;
+    
---
+++ client/src/pages/Register.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Register() {
+  return (
+    <div><h1>Register page </h1></div>
+  )
+}
