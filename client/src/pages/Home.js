9eb99d67737ab534feacf7a3bed1896b22f2894a
johad <johadtoledo@gmail.com>
2022-04-20 11:35:40 +0200

    Add register page with a form and mutations

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,43 +1,13 @@
 import React from 'react';
 import { useQuery } from '@apollo/react-hooks'
 import gql from 'graphql-tag';
-import { Grid } from 'semantic-ui-react';
+import { Grid, Transition } from 'semantic-ui-react';
 
 import PostCard from '../components/PostCard';
 
- export default function Home() {
-  
-  const {
-    loading,
-     data: { getPosts: posts }
-    }  = useQuery(FETCH_POSTS_QUERY);
-    
-    // const { loading, data: { posts } = {} } = useQuery(FETCH_POSTS_QUERY);
-
-  return (
-    <Grid columns={3}>
-    <Grid.Row className='page-title'>
-      <h1>Recent Posts</h1>
-    </Grid.Row>
-    <Grid.Row>
-      {loading ? 
-        (
-          <h1>Loading posts...</h1>
-        ):(
-          posts && posts.map(post =>(
-            <Grid.Column key={post.id} style={{marginBottom: 20}}>
-              <PostCard post={post} />
-            </Grid.Column>
-          ))
-        )}
-    </Grid.Row>
-    </Grid>
-  )
-}
-
 const FETCH_POSTS_QUERY = gql`
-   {
-  getPosts{
+   query GetPosts{
+  posts: getPosts{
     id
     body
     createdAt
@@ -58,3 +28,58 @@ const FETCH_POSTS_QUERY = gql`
   }
 }
   `
+
+ export default function Home() {
+  
+  // const { data, loading, error } = useQuery(FETCH_POSTS_QUERY);
+  // if(data) {
+  //   console.log(data);
+  //   const { getPosts: posts } = data;
+  // }
+  // if(error) {
+  //   console.log(error);
+  //   return "error"; // blocks rendering
+  // }
+
+  // const {
+  //   loading,
+  //    data: { getPosts: posts }
+  //   }  = useQuery(FETCH_POSTS_QUERY);
+    
+    const { loading, data, data: { posts } = {} } = useQuery(FETCH_POSTS_QUERY);
+
+
+  // {loading && <h1>Loading posts...</h1>}
+  // {data && (
+  //  <Transition.Group>
+  //    {posts &&
+  //     posts.map((post) => (
+  //       <Grid.Column key={post.id} style={{ marginBottom: 20 }}>
+  //         <PostCard post={post} />
+  //       </Grid.Column>
+  //     ))}
+  // </Transition.Group>
+  // )}
+
+
+  return (
+    <Grid columns={3}>
+    <Grid.Row className='page-title'>
+      <h1>Recent Posts</h1>
+    </Grid.Row>
+    <Grid.Row>
+    {loading ? 
+  (
+    <h1>Loading posts...</h1>
+  ):(
+    posts && 
+    posts.map(post =>(
+      <Grid.Column key={post.id} style={{marginBottom: 20}}>
+        <PostCard post={post} />
+      </Grid.Column>
+    ))
+  )}
+    </Grid.Row>
+    </Grid>
+  )
+}
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,7 +1,92 @@
-import React from 'react'
+import { useMutation } from '@apollo/client'
+import gql from 'graphql-tag'
+import React, { useState } from 'react'
+import { Button, Form } from 'semantic-ui-react'
 
 export default function Register() {
+  const [values, setValues] = useState({
+    username: '',
+    email:'',
+    password:'',
+    confirmPassword:'',
+  })
+
+  const onChange = (e) =>{
+    setValues({...values, [e.target.name]: e.target.value})
+  }
+
+  const [addUser, {loading}] = useMutation(REGISTER_USER,{
+    update(proxy, result){
+      console.log(result)
+    },
+    variables: values
+  })
+
+  const onSubmit = e =>{
+    e.preventDefault()
+    addUser()
+  }
+
+
   return (
-    <div><h1>Register page </h1></div>
+    <div className='form-container'>
+      <Form onSubmit={onSubmit} noValidate>
+        <h1>Register</h1>
+        <Form.Input
+          label='Username'
+          placeholder='Username..'
+          name='username'
+          type='text'
+          value={values.username}
+          onChange={onChange}
+          />
+        <Form.Input
+          label='Email'
+          placeholder='Email..'
+          name='email'
+          type='email'
+          value={values.email}
+          onChange={onChange}
+          />
+        <Form.Input
+          label='Password'
+          placeholder='Password..'
+          name='password'
+          type='password'
+          value={values.password}
+          onChange={onChange}
+          />
+          <Form.Input
+          label='Confirm Password'
+          placeholder='Confirm Password..'
+          name='confirmPassword'
+          type='password'
+          value={values.confirmPassword}
+          onChange={onChange}
+          />
+          <Button type='submit' primary>
+            Resgister
+          </Button>
+      </Form>
+    </div>
   )
 }
+
+const REGISTER_USER = gql`
+mutation register(
+  $username: String!
+  $email: String!
+  $password: String!
+  $confirmPassword: String!
+){
+  register(
+    registerInput:{
+      username: $username
+      email: $email
+      password: $password
+      confirmPassword: $confirmPassword
+    }
+  ){
+    id email username createdAt token
+  }
+}`
